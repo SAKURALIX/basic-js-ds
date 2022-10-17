b1708091e6f903f92198f56e331ab27c1358838c
SAKURALIX <sakura.xlix@gmail.com>
2022-10-18 02:52:05 +0300

    Remove from list corrected

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,12 +1,22 @@
-function removeKFromList({value, next}, k) {
-  if(value === k){
-    return next ? removeKFromList(next, k) : null;
-   } else {
-      return {
-        next : removeKFromList(next, k),
-        value
-      };
+function removeKFromList(l, k){
+  if(l == null){
+      return l;
   }
+  while(l.value == k){
+      l = l.next;
+  }
+  thisNode = l;
+  nextNode = thisNode.next;
+  while(nextNode != null){
+      if(nextNode.value == k){
+          thisNode.next = nextNode.next;
+          if(thisNode.next == null)
+              break;
+      }
+      thisNode = thisNode.next;
+      nextNode = thisNode.next;       
+  }
+  return l;
 }
 
 module.exports = {
